4c96ce18db6c6ba5d7ea5b016963ab4fc25f114b
powerrmt <powerrmt@gmail.com>
2014-09-30 21:26:30 -0400

    Fixed validateMove call in minimax

--- Offset/src/offset/minimax/Player.java
+++ Offset/src/offset/minimax/Player.java
@@ -37,81 +37,60 @@ public class Player extends offset.sim.Player {
     //BROUGHT OVER FROM GOMOKU
     //get timelimit for subtrees
     minimaxID = myid;
-    minimaxGrid = new ArrayList<Point>(Arrays.asList(grid.clone()));
     timelimit = ((TIME_OUT*1000))/(double)(getValidMoves(pr,grid).size());
     
     if (getValidMoves(pr,grid).size() < 5000){
       ISLATER = true;
-      bestMovePr = bestMove(minimaxGrid, SEARCH_DEPTH, pr, pr0, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY).getMovePair();
+      bestMovePr = bestMove(copyGrid(grid), SEARCH_DEPTH, pr, pr0, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY).getMovePair();
+      if (bestMovePr != null){
+        bestMovePr.move = true;
+        return bestMovePr;
       }
+    }
     else{
 
       for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
           for (int i_pr=0; i_pr<size; i_pr++) {
             for (int j_pr=0; j_pr <size; j_pr++) {
-              movepr.x = grid[i*size+j];
-              movepr.y = grid[size*i_pr+j_pr];
-              int score = validateMove(movepr, pr);
-              if (score > bestScore) {
-                bestScore = score;
-                bestMovePr.x = movepr.x; // deep copy
-                bestMovePr.y = movepr.y;
+              movepr.src = grid[i*size+j];
+              movepr.target = grid[size*i_pr+j_pr];
+              if (validateMove(movepr, pr)) {
+                      movepr.move = true;
+                      return movepr;
               }
             }
           }
         }
       }
 
-      if (bestScore > 0)
-        bestMovePr.move = true;
-    
     }
     return bestMovePr;
   }
 
+  boolean validateMove(movePair movepr, Pair pr) {
+    	
+    	Point src = movepr.src;
+    	Point target = movepr.target;
+    	boolean rightposition = false;
+    	if (Math.abs(target.x-src.x)==Math.abs(pr.p) && Math.abs(target.y-src.y)==Math.abs(pr.q)) {
+    		rightposition = true;
+    	}
+    	if (Math.abs(target.x-src.x)==Math.abs(pr.p) && Math.abs(target.y-src.y)==Math.abs(pr.q)) {
+    		rightposition = true;
+    	}
+        if (rightposition && src.value == target.value && src.value >0) {
+        	return true;
+        }
+        else {
+        	return false;
+        }
+    }
 
-  int validateMove(movePair movepr, Pair pr) {
-    Point src = movepr.x;
-    Point target = movepr.y;
-    boolean rightposition = false;
-
-    if (Math.abs(target.x-src.x)==Math.abs(pr.x) && Math.abs(target.y-src.y)==Math.abs(pr.y)) {
-      rightposition = true;
-    }
-    if (Math.abs(target.x-src.x)==Math.abs(pr.y) && Math.abs(target.y-src.y)==Math.abs(pr.x)) {
-      rightposition = true;
-    }
-    if (rightposition && src.value == target.value && src.value >0) {
-      return src.value;
-    }
-    else {
-      return 0;
-    }
-  }
-
-  int validateMove(movePair movepr, Pair pr, ArrayList<Point> grid) {
-    Point src = grid.get(grid.indexOf(movepr.x));
-    Point target = grid.get(grid.indexOf(movepr.y));
-    boolean rightposition = false;
-
-    if (Math.abs(target.x-src.x)==Math.abs(pr.x) && Math.abs(target.y-src.y)==Math.abs(pr.y)) {
-      rightposition = true;
-    }
-    if (Math.abs(target.x-src.x)==Math.abs(pr.y) && Math.abs(target.y-src.y)==Math.abs(pr.x)) {
-      rightposition = true;
-    }
-    if (rightposition && src.value == target.value && src.value >0) {
-      return src.value;
-    }
-    else {
-      return 0;
-    }
-  }
   //Implementation of alpha-beta minimax
-  protected ScoredMove bestMove(ArrayList<Point> grid, int depth, Pair myPair, Pair herPair, double mybest, double herbest){
+  protected ScoredMove bestMove(Point[] grid, int depth, Pair myPair, Pair herPair, double mybest, double herbest){
     ArrayList<movePair> moveList = new ArrayList<movePair>();
-    ArrayList<Point> tempGrid;
+    Point[] tempGrid = new Point[grid.length];
     movePair lastMove;
     double bestscore = 0.0;
     movePair bestMove = new movePair();
@@ -119,8 +98,10 @@ public class Player extends offset.sim.Player {
     double tempscore = 0.0;
     double divtime = 0.0;
 
+    double timebefore, timeafter;
+
     //Score for this player on current board
-    int thisBoardScore = calculateScore(minimaxID,grid.toArray(new Point[0]));
+    int thisBoardScore = calculateScore(minimaxID,grid);
 
     //The id of this player
     int playerID = minimaxID;
@@ -135,13 +116,14 @@ public class Player extends offset.sim.Player {
     }
 
     //Get set of valid moves for this player
-    moveList = getValidMoves(myPair,tempGrid.toArray(new Point[0]));
+    moveList = getValidMoves(myPair,tempGrid);
 
     //Set alpha-beta params
     bestscore = mybest;
     bestMove = null;
 
     //Iterate over movelist until we find the best one
+    timebefore = (double)System.currentTimeMillis();
     for (int i = 0; i < moveList.size(); i++){
 
       //Timing is enforced here so we don't spend to long searching a subtree
@@ -159,7 +141,7 @@ public class Player extends offset.sim.Player {
       movePair chosenMove = moveList.get(i);
 
       //Make the chosen move on our copied grid
-      ArrayList<Point> newTempGrid = makeHypMove(minimaxID,tempGrid,chosenMove);
+      Point[] newTempGrid = makeHypMove(minimaxID,tempGrid,chosenMove);
 
       //Need to set which player is moving next
       if (minimaxID == 1)
@@ -177,6 +159,7 @@ public class Player extends offset.sim.Player {
       //Negate opponent's best score
       tempscore = -1 * tempmove.getScore();
 
+
       //check if opponent's best score is lower than scores we found
       //previously (we do > because we negated their score)
       if (tempscore > bestscore){
@@ -189,6 +172,8 @@ public class Player extends offset.sim.Player {
         return new ScoredMove(bestscore, bestMove);
       }
     }
+    timeafter= (double)System.currentTimeMillis();
+
 
     //We checked every possible move, so return the best one
     return new ScoredMove(bestscore, bestMove);
@@ -211,17 +196,18 @@ public class Player extends offset.sim.Player {
   //pair on that board.
   ArrayList<movePair> getValidMoves(Pair myPair, Point[] grid){
     movePair movepr = new movePair();
+    movepr.move = false;
     ArrayList<movePair> moveList = new ArrayList<movePair>();
     for (int i = 0; i < size; i++) {
       for (int j = 0; j < size; j++) {
         for (int i_pr=0; i_pr<size; i_pr++) {
           for (int j_pr=0; j_pr <size; j_pr++) {
-            movepr.x = grid[i*size+j];
-            movepr.y = grid[size*i_pr+j_pr];
-            int score = validateMove(movepr, myPair);
-            if (score > 0) {
+            movepr.src = grid[i*size+j];
+            movepr.target = grid[size*i_pr+j_pr];
+            if (validateMove(movepr,myPair)) {
               moveList.add(movepr);
-              movepr = new movePair();
+              if (ISLATER)
+                return moveList;
             }
           }
         }
@@ -232,28 +218,39 @@ public class Player extends offset.sim.Player {
   }
 
   //Given a board and a hypothetical move, update the grid to take that move into account.
-  ArrayList<Point> makeHypMove(int id, ArrayList<Point> grid, movePair movepr){
-    if (movepr.move) {
-      Point src = grid.get(grid.indexOf(movepr.x));
-      Point target = grid.get(grid.indexOf(movepr.y));
-      target.value = target.value+src.value;
-      src.value = 0;
-      target.owner = id;
-      src.owner = -1;
-      src.change = true;
-      target.change = true;
+  Point[] makeHypMove(int id, Point[] grid, movePair movepr){
+    Point[] newGrid = copyGrid(grid);
+    Point src = new Point();
+    Point target = new Point();
+    int srcSpot, targetSpot;
+    srcSpot = -1;
+    targetSpot = -1;
+    for (int i = 0; i < grid.length; i ++){
+      if (newGrid[i].x == movepr.src.x && newGrid[i].y == movepr.src.y){
+       src = new Point(movepr.src.x,movepr.src.y,movepr.src.value,movepr.src.owner); 
+       srcSpot = i;
+      }
+      if (newGrid[i].x == movepr.target.x && newGrid[i].y == movepr.target.y){
+       target = new Point(movepr.target.x,movepr.target.y,movepr.target.value,movepr.target.owner); 
+       targetSpot = i;
+      } 
     }
-    return grid;
+    target.value = target.value+src.value;
+    src.value = 0;
+    target.owner = id;
+    src.owner = -1;
+    src.change = true;
+    target.change = true;
+    newGrid[srcSpot] = src;
+    newGrid[targetSpot] = target;
+    return newGrid;
   }
 
   //Make copy of grid contents
-  ArrayList<Point> copyGrid (ArrayList<Point> grid){
-    ArrayList<Point> newGrid = new ArrayList<Point>();
-    for (int i = 0; i < grid.size(); i++){
-      Point newPoint = new Point();
-      newPoint.x = grid.get(i).x;
-      newPoint.y = grid.get(i).y;
-      newGrid.add(newPoint);
+  Point[] copyGrid (Point[] grid){
+    Point[] newGrid = new Point[grid.length];
+    for (int i = 0; i < grid.length; i++){
+      newGrid[i] = new Point(grid[i].x,grid[i].y,grid[i].value,grid[i].owner);
     }
     return newGrid;
   }
